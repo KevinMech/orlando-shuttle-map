f8da417e3291c795209ff55c07b0a7d154cb820d
KevinMech <kevin.mellachew@gmail.com>
2018-03-21 06:41:24 -0400

    Add Default Parameters to Map Component

--- src/components/map.js
+++ src/components/map.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import '../App.css';
-import ReactMapBoxGl from 'react-mapbox-gl';
+import ReactMapBoxGl, {ZoomControl} from 'react-mapbox-gl';
 import Token from '../tokens.json'
 
 const Map = ReactMapBoxGl({
     accessToken: Token.mapbox
 });
 
-class MapBox extends React.Component{
-    constructor(props){
-        super();
-    }
-
-    DidComponentMount(){
-    }
-
-    render() {
-        return(
-            <div className='Map'>
-                <Map style='mapbox://styles/mapbox/streets-v10' containerStyle={{height: '800px', width: '100%'}}/>
-            </div>
-        );
-    }
+const MapBox = () => {
+    return(
+        <div className='Map'>
+            <Map style='mapbox://styles/mapbox/streets-v10' containerStyle={{height: '800px', width: '100%'}} center={[-81.37923649999999, 28.5383355]} zoom={[11]}/>
+        </div>
+    );
 }
 
 export default MapBox;
