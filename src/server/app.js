48227f551712576b928fdf373005e69643402834
KevinMech <kevin.mellachew@gmail.com>
2018-05-10 14:24:53 -0400

    Add event listener for incoming connections

--- src/server/app.js
+++ src/server/app.js
@@ -1,1 +1,13 @@
-console.log('test');
+const express = require('express');
+const app = express();
+
+const config = require('./config.json');
+
+app.get('/', (req, res) => {
+    console.log('Get request received!');
+    res.send('Welcome!');
+});
+
+app.listen(config.port, () => {
+    console.log(`now listening on port ${config.port}...`);
+});
