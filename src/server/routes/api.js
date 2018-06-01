35d5affe8cd403f216966d6b90fdd7db2119c207
KevinMech <kevin.mellachew@gmail.com>
2018-06-01 03:38:39 -0400

    Scrap Server

--- src/server/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/routes', (req, res) => {
-    console.log(`Get requested for ${req.path} from ${req.ip}!`);
-    res.send('Routes here');
-});
-
-module.exports = router;
