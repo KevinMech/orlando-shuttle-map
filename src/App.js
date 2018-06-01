3d2e9cb7faff77fab5260b615c3cdf09a9786323
KevinMech <kevin.mellachew@gmail.com>
2018-06-01 16:56:03 -0400

    Add geoJSON

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './config/busroutes.json';
 import Header from './components/header.js';
 import MapArea from './components/maparea.js';
 
