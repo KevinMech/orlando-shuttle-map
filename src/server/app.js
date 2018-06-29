d1445acb7a2b0c16fce7ec0cb155af6a9c6f8a08
KevinMech <kevin.mellachew@gmail.com>
2018-06-29 15:12:40 -0400

    Add functionality to connect to database

--- src/server/app.js
+++ src/server/app.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const apiroutes = require('./routes/api');
 const config = require('./config.json');
+const {Client} = require('pg');
+
 
 //use port defined by Heroku, otherwise use port defined in config
 let port = process.env.PORT || config.port;
+
+//Connect to Database
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
+
+client.connect((err) =>{
+    if(err) console.log("Error connecting to database! Error:${err");
+    else console.log("Connected to database successfully!");
+});
 
 app.get('/', (req, res) => {
     console.log(`Get requested for ${req.path} from ${req.ip}!`);
