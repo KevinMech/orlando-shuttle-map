ade412946bfd1fc6313d7d631d441220ee1a210f
kevinmech <>
2018-08-08 14:48:24 -0400

    Add Airbnb styleguide to linter

--- src/server/lib/db.js
+++ src/server/lib/db.js
@@ -1,31 +1,30 @@
-const {Client} = require('pg');
+const { Client } = require('pg');
 
 const client = new Client({
     user: 'kevin',
     host: 'localhost',
     database: 'bus',
-    port: '5432'
+    port: '5432',
 });
 
-exports.connect = async() =>{
+exports.connect = async () => {
     try {
-        client.connect(() =>{
+        await client.connect(() => {
             console.log('Connected to database successfully!');
         });
         return true;
     } catch (err) {
         console.log('Error connecting to database!');
         console.log(err);
-        return false;    
+        return false;
     }
-}
+};
 
-exports.addBusRoute = async(name) =>{
-    try{
+exports.addBusRoute = async (name) => {
+    try {
         client.query(`INSERT INTO bus(name) VALUES('${name}')`);
-    }
-    catch(err){
+    } catch (err) {
         console.log('Failed to add bus route to database!');
         console.log(err);
     }
-}
+};
