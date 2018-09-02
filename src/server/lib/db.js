aa6ef096fdd18ec1d97e075f557b578e7d8b975d
kevinmech <>
2018-09-02 16:32:10 -0400

    Add readfiles function

--- src/server/lib/db.js
+++ src/server/lib/db.js
@@ -2,6 +2,7 @@ const { Client } = require('pg');
 const fs = require('fs');
 const crypto = require('crypto');
 const { promisify } = require('util');
+const test = require('../geojson/route13.json');
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
@@ -15,6 +16,7 @@ function File(name, hash) {
 
 function hashFile(file, directory) {
     return new Promise((resolve) => {
+        console.log('Hashing file...');
         const hash = crypto.createHash('md5');
         let hashed;
         hash.setEncoding('hex');
@@ -29,15 +31,19 @@ function hashFile(file, directory) {
     });
 }
 
-function hashFiles(files, directory) {
+function readFiles(filenames, directory) {
     return new Promise((resolve) => {
+        console.log('Reading Files from directory...');
         const promises = [];
-        files.forEach((file) => {
-            console.log(`Hashing ${file}...`);
-            promises.push(hashFile(file, directory));
+        let filecntr = 0;
+        filenames.forEach((filename) => {
+            console.log(`Reading ${filename}...`);
+            let file = require('.' + directory + filename);
+            promises.push(hashFile(filename, directory));
+            filecntr += 1;
         });
         Promise.all(promises).then((result) => {
-            console.log('hashing complete!');
+            console.log(`Finished reading ${filecntr} files!`);
             resolve(result);
         });
     });
@@ -74,9 +80,8 @@ exports.connect = () => new Promise((resolve, reject) => {
 exports.poppulate = () => new Promise(async (resolve) => {
     const directory = './geojson/';
     const readdir = await promisify(fs.readdir);
-    console.log('Reading Files from directory...');
-    const files = await readdir(directory);
-    const hashedfiles = await hashFiles(files, directory);
-    await addBusRoutes(hashedfiles);
+    const filenames = await readdir(directory);
+    const files = await readFiles(filenames, directory);
+    await addBusRoutes(files);
     resolve();
 });
